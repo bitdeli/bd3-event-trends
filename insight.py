2f5d26f040704f8a4fbff0a00c933a3a5a53ec6e
ville <ville@bitdeli.com>
2013-02-12 04:37:36 +0000

    implement basic trends

--- insight.py
+++ insight.py
@@ -1,14 +1,40 @@
+from collections import Counter
+from datetime import datetime, timedelta
+
 from bitdeli.insight import insight
-from bitdeli.widgets import Text, Bar, Table
-from discodb.query import Q, Literal, Clause
+from bitdeli.widgets import Line
+
+NUM_DAYS = 30
+MAX_EVENTS = 3
+
+def get_latest(model):
+    return max(key.split(':', 1)[0] for key in model)
+
+def get_events(model):
+    return frozenset(key.split(':', 1)[1] for key in model)
+
+def daily_count(event, day, model):
+    return sum(int(value.split(':', 1)[0])
+               for value in model.get('%s:%s' % (day, event), []))
+
+def choose_events(model):
+    latest = get_latest(model)
+    events = get_events(model)
+    if len(events) > MAX_EVENTS:
+        c = Counter((event, daily_count(event, latest, model))
+                    for event in events)
+        events = frozenset(x[0] for x in c.most_common(MAX_EVENTS))
+    return latest, events
+
+def trend(event, latest_day, model):
+    for i in range(NUM_DAYS):
+        day = (latest_day - timedelta(days=i)).isoformat()
+        yield day, daily_count(event, day.split('T')[0].replace('-', ''), model)
 
 @insight
 def view(model, params):
-    params = {'events': ['$signup', 'Clip created']}
-    def steps(events):
-        for i in range(len(events)):
-            q = Q([Clause([Literal(event)]) for event in events[:i + 1]])
-            yield events[i], len(model.query(q))
-    return [Bar(size=(12, 6),
-                data=list(steps(params['events'])))]
-    
+    latest, events = choose_events(model)
+    latest_day = datetime.strptime(latest, '%Y%m%d')
+    data = [{'label': event, 'data': list(trend(event, latest_day, model))}
+            for event in events]
+    return [Line(size=(12, 6), data=data)]
--- model.py
+++ model.py
@@ -1,7 +1,16 @@
+from datetime import datetime
+from itertools import groupby
 from bitdeli.model import model
+
+def day(hours):
+    for hour, count in hours:
+        yield datetime.utcfromtimestamp(hour * 3600).strftime('%Y%m%d'), count
 
 @model
 def build(profiles):
     for profile in profiles:
-        for event in profile['events']:
-            yield event, profile.uid
+        uid = profile.uid
+        for event, hours in profile['events'].iteritems():
+            for date, counts in groupby(day(hours), lambda x: x[0]):
+                yield '%s:%s' % (date, event),\
+                      '%d:%s' % (sum(count for date, count in counts), uid)
