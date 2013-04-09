bbf599f26bad01cd8101148b0bf7347dde986f5f
Jyri Tuulos <test24@example.com>
2013-04-09 23:38:12 +0000

    add initial support for segment creation
    TODO: handle selected segments

--- insight.py
+++ insight.py
@@ -6,6 +6,8 @@ from bitdeli.widgets import Line, Text, Widget
 
 NUM_DAYS = 30
 MAX_EVENTS = 3
+SEGMENT_RANGE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
+SEGMENT_LABEL_FORMAT = "%b %d, %Y"
 
 class TokenInput(Widget):
     pass
@@ -17,6 +19,10 @@ def unique(events):
             yield event
             seen.add(event)
 
+def get_chosen(params, max_events=MAX_EVENTS):
+    return list(unique(params['events']['value']
+                if 'events' in params else []))[:max_events]
+            
 def get_latest(model):
     return max(key.split(':', 1)[0] for key in model)
 
@@ -34,8 +40,7 @@ def trend(event, latest_day, model):
 
 @insight
 def view(model, params):
-    chosen = list(unique(params['events']['value']
-                  if 'events' in params else []))[:MAX_EVENTS]
+    chosen = get_chosen(params)
 
     yield Text(size=(12, 'auto'),
                label='Analyzing event trends',
@@ -55,10 +60,36 @@ def view(model, params):
                    size=(12, 6),
                    data=data)
 
+
+def get_segment_dates(timestamps):
+    return (datetime.strptime(tstamp, SEGMENT_RANGE_FORMAT)
+            for tstamp in timestamps)
+
+def daily_users(event, day, model):
+    return (value.split(':', 1)[1]
+            for value in model.get('%s:%s' % (day, event), []))
+
 @segment
 def segment(model, params):
-    return ['1', '1000105556']
-
+    chosen = get_chosen(params['params'])
+    start, end = get_segment_dates(params['value'])
+    users = set()
+    for event in chosen:
+        for i in range((end - start).days + 1):
+            day = (end - timedelta(days=i)).strftime("%Y%m%d")
+            users.update(daily_users(event, day, model))                
+    return users
+ 
 @segment_label
-def label(segment, params):
-    return '%d people' % len(segment)
+def label(segment, model, params):
+    chosen = get_chosen(params['params'])
+    start, end = get_segment_dates(params['value'])
+    
+    label = 'Users who did %s ' % ', '.join(chosen)
+    dateformat = SEGMENT_LABEL_FORMAT
+    if end - start:
+        label = label + 'between %s and %s' % (start.strftime(dateformat),
+                                                end.strftime(dateformat))
+    else:
+        label = label + 'on %s' % start.strftime(dateformat)
+    return label
