9f9f5f7bbbf04fca569e234e8bee1d536f3d8331
Jyri Tuulos <jyri.tuulos@gmail.com>
2013-04-30 23:16:38 +0000

    include dom events in jsapi model

--- jsapi/model.py
+++ jsapi/model.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from itertools import groupby, islice
+from itertools import groupby, islice, chain
 from bitdeli.model import model, uid
 from collections import Counter
 
@@ -8,17 +8,27 @@ NUM_DAYS = 30
 @uid
 def uid(value):
     return value[value.find(':') + 1:]
-        
+
+
+def event_names(events):
+    for tstamp, group, ip, event in events:
+        name = event.get('$event_name', None)
+        if name == '$dom_event':
+            name = event.get('$event_label', None)
+        if name:
+            yield name
+
 @model
 def build(profiles):
     def parse_day(event):
         return event[0][:10].replace('-', '')
     for profile in profiles:
         uidd = profile.uid
-        days = groupby(profile['events'], parse_day)
+        source_events = chain(profile.get('events', []),
+                              profile.get('$dom_event', []))
+        days = groupby(source_events, parse_day)
         for day, events in islice(days, NUM_DAYS):
-            c = Counter(event['$event_name']
-                        for tstamp, group, ip, event in events)
+            c = Counter(event_names(events))
             for event, count in c.iteritems():
                 yield '%s:%s' % (day, event),\
                       '%d:%s' % (count, uidd) 
